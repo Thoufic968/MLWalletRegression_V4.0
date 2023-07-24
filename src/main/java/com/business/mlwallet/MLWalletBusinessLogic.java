f393c3edd3ac70470680fcdabff063ebe3b5b7a9
tsgadmin <tsgadmin@HOTGQA0002.MLHUILLIER.LOCAL>
2023-07-24 15:05:46 +0800

    Sprint 8

--- src/main/java/com/business/mlwallet/MLWalletBusinessLogic.java
+++ src/main/java/com/business/mlwallet/MLWalletBusinessLogic.java
@@ -22,7 +22,7 @@ import java.util.List;
 
 import static com.utility.Utilities.*;
 import static com.utility.Utilities.explicitWaitVisible;
-// Sprint demo 7
+// Sprint demo 8
 
 
 public class MLWalletBusinessLogic {
